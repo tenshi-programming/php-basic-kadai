38dd2df89ffbee89e6f102214e62cb0d7445fa64
tenshi-programming <165974443+tenshi-programming@users.noreply.github.com>
2024-06-01 15:37:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP課題「連想配列を作って値とキーを出力しよう」</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            $personal_date = ['name'=>'onion','price'=>'200','weight'=>'160'];
+            print_r($personal_date);
+            ?>
+        </p>
+    </body>
+</html>
